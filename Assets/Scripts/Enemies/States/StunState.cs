b8307e512032a915f1ffcb7b34445ae8b343d87f
phongtruong2106 <thpgamer42@gmail.com>
2023-02-12 12:52:56 +0700

    update

--- Assets/Scripts/Enemies/States/StunState.cs
+++ Assets/Scripts/Enemies/States/StunState.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StunState : MonoBehaviour
+public class StunState : State
 {
-    // Start is called before the first frame update
-    void Start()
+    protected D_StunState stateData;
+    protected bool isStunTimeOver; //dung choang
+    public StunState(Entity entity, FiniteStateMachine stateMachine, string animBoolName, D_StunState stateData) : base(entity, stateMachine, animBoolName)
     {
-        
+        //gan du lieu trang thai
+        this.stateData = stateData;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void DoChecks()
     {
-        
+        base.DoChecks();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        isStunTimeOver = false; //ngay luc nay se khong bi choang => viec choang se bi tam hoan
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        if(Time.time >= startTime +  stateData.stunTime)
+        {
+            isStunTimeOver = true;
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
     }
 }
